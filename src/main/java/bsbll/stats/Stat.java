a56b4e62595bcaf2a134505c39d0b4da0be172d4
tzeth <jetoze@gmail.com>
2018-06-01 08:32:14 -0700

    Added comment about adding a get method to Stat.

--- src/main/java/bsbll/stats/Stat.java
+++ src/main/java/bsbll/stats/Stat.java
@@ -9,6 +9,14 @@ public interface Stat<T> {
     // TODO: Add this
     // String abbrev();
     
+    // TODO: I would like a generic declaration of the get(StatLine) method here, but
+    // I don't know yet how to pull that off. The generic trickery becomes rather
+    // immense, unless I'm missing something obvious. The problem is a circular
+    // type dependency: Stat would have a generic type referring to StatLine, 
+    // which has a generic type referring back to Stat.
+    // Maybe what's suggested in this post will work:
+    //    https://stackoverflow.com/questions/3147308/cyclic-generic-dependenices
+    
     /**
      * The {@code PrimitiveStat}s are the individual stats that are actually
      * counted. Other stats can then be composed from two or more primitive
