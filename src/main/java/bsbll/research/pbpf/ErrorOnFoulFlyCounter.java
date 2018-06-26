f90320c2e77ac43985498d4f5e98503d0298f4ca
tzeth <jetoze@gmail.com>
2018-06-26 06:22:45 -0700

    Turned the ErrorOnFoulFlyCounter into a distribution.

--- src/main/java/bsbll/research/pbpf/ErrorOnFoulFlyCounter.java
+++ src/main/java/bsbll/research/pbpf/ErrorOnFoulFlyCounter.java
@@ -1,8 +1,9 @@
 package bsbll.research.pbpf;
 
-import java.text.DecimalFormat;
-
+import com.google.common.collect.HashMultiset;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
 
 import bsbll.Year;
 import bsbll.game.play.EventType;
@@ -18,21 +19,37 @@ public final class ErrorOnFoulFlyCounter extends GameHandler {
     //    + Number of times the event happened twice during a plate appearance;
     //    + Etc
     private int plateAppearances;
-    private int errors;
+    private final Multiset<Integer> distribution = HashMultiset.create();
     
     @Override
     public void onEndOfInning(Inning inning, ImmutableList<ParsedPlay> plays) {
         PlayerId lastBatter = null;
+        int eventsDuringPA = 0;
         for (ParsedPlay play : plays) {
             if (lastBatter == null || !play.getBatterId().equals(lastBatter)) {
                 // This logic is not correct, since a plate appearance can span over
                 // multiple innings (e.g. the last out is made on a caught stealing).
                 ++plateAppearances;
+                if (eventsDuringPA > 0) {
+                    distribution.add(eventsDuringPA);
+                }
                 lastBatter = play.getBatterId();
+                eventsDuringPA = 0;
             }
             if (play.getType() == EventType.ERROR_ON_FOUL_FLY) {
-                ++errors;
+                ++eventsDuringPA;
             }
+        }
+        if (eventsDuringPA > 0) {
+            distribution.add(eventsDuringPA);
+        }
+    }
+    
+    public void report(Year year) {
+        System.out.println("Error on Foul Fly Distribution in " + year);
+        System.out.println("Plate Appearances: " + plateAppearances);
+        for (Multiset.Entry<Integer> e : Multisets.copyHighestCountFirst(distribution).entrySet()) {
+            System.out.println(String.format("%s: %s time(s)", e.getElement(), e.getCount()));
         }
     }
     
@@ -40,8 +57,6 @@ public final class ErrorOnFoulFlyCounter extends GameHandler {
         Year year = Year.of(1925);
         ErrorOnFoulFlyCounter counter = new ErrorOnFoulFlyCounter();
         counter.parseAll(PlayByPlayFileUtils.getFolder(year));
-        System.out.println("Plate Appearances: " + counter.plateAppearances);
-        System.out.println("Erorr on Foul Fly Occurences: " + counter.errors);
-        System.out.println("Pct: " + new DecimalFormat("0.0000").format((1.0 * counter.errors) / counter.plateAppearances));
+        counter.report(year);
     }
 }
